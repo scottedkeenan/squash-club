8eabcc1c3ebf3a102a3c6ed9a80332538013325a
Scott <scottsimuk@gmail.com>
2019-09-19 22:31:01 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+	$servername = "localhost";
+	$username = "clubadmin";
+	$password = "aZKAoXzGFVDlPGxz";
+	$dbname = "database";
+	$tablename = "bookingcalendar";
+
+	// translate these
+	$months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+	$headings = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+?>
