16adb8fea6409ddd1948a31474543901fc4f6a5c
Scott <scottsimuk@gmail.com>
2020-03-15 11:56:07 +0000

    Reset modal when clicking elsewhere

--- index.php
+++ index.php
@@ -186,14 +186,20 @@ btn.onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
+    $('#modal_message').text("Please scan your fob now");
+    $('#proceed_to_book').hide();
+    $('#username').val(null);
 }
 
 // When the user clicks anywhere outside of the modal, close it
 //todo: also reset the modal
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+      modal.style.display = "none";
+      $('#modal_message').text("Please scan your fob now");
+      $('#proceed_to_book').hide();
+      $('#username').val(null);    
   }
 }
 </script> 
